1792049fa06fc978b7f458490b9bbe78e4b06f18
Balla <ballaphanindra@outlook.com>
2022-06-11 21:59:35 +0530

    initial commit

---
+++ LineComp/LineComp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
